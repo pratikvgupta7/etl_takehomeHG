56b06167ee756d82b26770692e55bd03503b64cb
Pratik Gupta <pvgaqu@gmail.com>
2026-03-11 11:38:15 -0700

    Fix

--- orchestration/flow.py
+++ orchestration/flow.py
@@ -4,45 +4,62 @@ os.environ["PREFECT_API_URL"] = "http://prefect-server:4200/api"
 
 from prefect import flow, task
 import subprocess
-
+from prefect import get_run_logger
 
 @task(retries=2, retry_delay_seconds=30)
 def ingest():
+    logger = get_run_logger()
+
     result = subprocess.run(
-        ["python", "/app/ingest.py"],
+        ["python", "ingest.py"],
+        cwd="/workspace/app",
         capture_output=True,
         text=True,
     )
-    print(result.stdout)
+
+    logger.info("STDOUT:\n%s", result.stdout)
+    logger.info("STDERR:\n%s", result.stderr)
+    logger.info("Return code: %s", result.returncode)
+
     if result.returncode != 0:
-        print(result.stderr)
-        raise RuntimeError("Ingestion failed")
+        raise RuntimeError(result.stderr)
 
 
 @task(retries=2, retry_delay_seconds=30)
 def dbt_run():
+    logger = get_run_logger()
     result = subprocess.run(
-        ["dbt", "run", "--project-dir", "/dbt_project", "--profiles-dir", "/dbt_project"],
+        ["dbt", "run"],
+        cwd="/workspace/dbt_project",
+        env=os.environ,
         capture_output=True,
         text=True,
     )
-    print(result.stdout)
+
+    logger.info(result.stdout)
+
     if result.returncode != 0:
-        print(result.stderr)
-        raise RuntimeError("dbt run failed")
+        logger.error(result.stderr)
+        raise RuntimeError(result.stderr)
 
 
 @task(retries=2, retry_delay_seconds=30)
 def dbt_test():
+    logger = get_run_logger()
+
     result = subprocess.run(
-        ["dbt", "test", "--project-dir", "/dbt_project", "--profiles-dir", "/dbt_project"],
+        ["dbt", "test"],
+        cwd="/workspace/dbt_project",
+        env=os.environ,
         capture_output=True,
         text=True,
     )
-    print(result.stdout)
+
+    logger.info(result.stdout)
+
     if result.returncode != 0:
-        print(result.stderr)
-        raise RuntimeError("dbt test failed")
+        logger.error(result.stderr)
+        raise RuntimeError(result.stderr)
 
 
 @flow(name="customer-churn-elt")
